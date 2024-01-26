89f5d876e7420c9e336d97cf14165eee2958094b
KevinRichardDev <kevin.richard0@orange.fr>
2024-01-26 05:55:14 +0100

    XMLHttpRequest

--- cours/index.js
+++ cours/index.js
@@ -1,1 +1,13 @@
+// XMLHttpRequest
+
+function reqListener() {
+  console.log(this.responseText);
+}
+
+let req = new XMLHttpRequest();
+req.onload = reqListener;
+// req.open('get', 'data.txt', true);
+// req.open("get", "data.json", true);
+req.open("get", "https://api.blablagues.net/?rub=blagues", true);
+req.send();
 
