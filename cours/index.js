61a3b0031ce64fbea304545e71fed8d407ed3a95
KevinRichardDev <kevin.richard0@orange.fr>
2024-01-28 08:13:15 +0100

    server json

--- cours/index.js
+++ cours/index.js
@@ -27,7 +27,14 @@ fetch("data.json")
   .then((res) => res.json())
   // .then((data) => console.log(data));
 
+const myHeaders = new Headers();
+
 const init = {
   method: "GET",
-
+  headers: myHeaders,
+  mode: "cors",
 }
+
+// fetch("data.json", init).then((res) => console.log(res));
+
+// CRUD => Create (POST), read (GET), update (PUT), Delete (DELETE)
